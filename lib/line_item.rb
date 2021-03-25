db7cea187e97a2cd708af606790c89bde69d22f1
anagha <anagha1996@gmail.com>
2021-03-25 18:40:04 +0530

    Create a class inbetween cart and item.

---
+++ lib/line_item.rb
@@ -0,0 +1,16 @@
+class LineItem
+
+  # Line Item is associated with both item and the cart. 
+  # This class will have code from one item in the store 
+  # and the price of it which can be changed due to any 
+  # offers applied.
+  
+  # Access to read code and price outside the class.
+  attr_reader :code
+  attr_accessor :price
+
+  # Initialize the Line Item Object
+  def initialize(code, price)
+    @code, @price = code, price
+  end
+end
